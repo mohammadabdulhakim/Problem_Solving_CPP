1baf0c31757dab66c6bb91cad77886770119ddca
mohammadabdulhakim <duxwork0@gmail.com>
2024-10-18 19:55:02 +0300

    4A watermelon

---
+++ Problem_Solving/codeforeces/4A_watermelon.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int weight;
+    cin >> weight; 
+
+    if(weight % 2 == 0 && weight != 2 && weight != 0 && weight != -2){
+        cout << "YES";
+        return 0;
+    }
+
+    cout << "NO";
+    return 0;
+}
