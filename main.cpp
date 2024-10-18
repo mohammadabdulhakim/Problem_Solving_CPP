56435fa75c5a5665e6f2e3e8ffb16e788ae472db
mohammadabdulhakim <duxwork0@gmail.com>
2024-10-18 09:17:39 +0300

    variables

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-    cout << "How are you," << endl << "\t \t Ahmed?";
+int main(){
+    int a, b, c;
+
+    a = 10, b = 50, c = 20;
+
+    cout << "sum = " << a + b + c;
 
     return 0;
 }
