76375b3cb1a6784874b2c998933fa1cce4b63db3
mohammadabdulhakim <duxwork0@gmail.com>
2024-10-18 15:46:20 +0300

    ASCII Decimal to ASCII Value

--- main.cpp
+++ main.cpp
@@ -1,51 +1,14 @@
-/*
-  Data Types
-
-  - Ram [R]andom [A]ccess [M]emory
-  --- Computer Memory Has Locations
-
-  - Data Sizes
-  --- Bit => [Bi]nary Digi[t] => Smallest Unit in Storage Store Only 0 Or 1
-  --- Byte => A Group of 8 => Store Single Character
-  --- Kilo Byte => 1024 Byte
-  --- Mega Byte => 1024 Kilobytes
-  --- Giga Byte => 1024 Megabytes
-  --- Tera Byte => 1024 Gigabytes
-
-  - Data Types With Size
-  --- int => 2 Or 4 Bytes => Will Cover Later Why
-  --- float => 4 Bytes [18.5656565656]
-  --- double => 8 Bytes [18.5656565656]
-  ------ Number . Fractional Component
-  --- char => 1 Byte => "A" "x" "9"
-  --- boolean => 1 Byte => true, false
-  - Double vs Float
-
-  - The Story Behind Creating A Variable
-  --- Declare A Variable
-  --- Tell Computer That We Need To Reserve X Bytes Of Memory Depend On Data Type
-  --- Restrict Type Of Data To The Type We Choosed
-
-  - Declare Variable Without Type
-  - See Memory Address &
-*/
-
-
-
 #include<iostream>
 using namespace std;
 
-int main(){
+int main () {
 
-    cout << "Enter the number of kilobytes: ";
-    int kilobytes;
-    cin >> kilobytes;
+  cout << "ASCII Decimal : ";
+  
+  int ASCIIDecimal;
+  cin >> ASCIIDecimal;
 
-    int bytes = kilobytes * 1024;
-    int bits = bytes * 8;
+  cout << "ASCII value : " << char(ASCIIDecimal) << ".\n";
 
-    cout << kilobytes << " kilobytes = " << bytes << " Bytes = " << bits << " Bits." << endl;
-
-
-    return 0;
+  return 0;
 }
