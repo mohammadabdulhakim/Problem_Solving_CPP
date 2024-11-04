7c470acb1636ad9a0cab8c0bf4afb13df036eab4
mohammadabdulhakim <duxwork0@gmail.com>
2024-11-04 20:39:11 +0200

    Calc with switch cases

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int floor;
-    cin >> floor;
+int main()
+{
+    int n1, n2, ope;
+    cout << "Enter your first number\n";
+    cin >> n1;
+    cout << "Enter your first number\n";
+    cin >> n2;
+    cout << "Enter the operation\n 1 > [+]\n 2 > [-]\n 3 > [*] \n 4 > [/]\n";
+    cin >> ope;
 
-    switch(floor){
-        case 1:
-        case 2:
-            cout << "Far";
-            break;
-        case 3:
-        case 4:
-            cout << "Almost";
-            break;
-        case 5:
-            cout << "Here";
+    switch (ope)
+    {
+    case 1:
+        cout << n1 << " + " << n2 << " = " << n1 + n2;
+        break;
+    case 2:
+        cout << n1 << " - " << n2 << " = " << n1 - n2;
+        break;
+    case 3:
+        cout << n1 << " * " << n2 << " = " << n1 * n2;
+        break;
+    case 4:
+        cout << n1 << " / " << n2 << " = " << n1 / n2;
+        break;
+    default:
+        cout << "Choose a valid operation";
     }
-    
+
+    cout << endl;
+
     return 0;
 }
